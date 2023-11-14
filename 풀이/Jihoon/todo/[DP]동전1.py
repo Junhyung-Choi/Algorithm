a0a61dc7899aa1059a809436dbf4c75a27538574
jihoon7712 <wlgns7711@hanyang.ac.kr>
2023-11-14 19:42:03 +0900

    동전 1.py

--- 풀이/Jihoon/todo/[DP]동전1.py
+++ 풀이/Jihoon/todo/[DP]동전1.py
@@ -1,2 +1,18 @@
 # 본인이 선택한 언어로 풀이해주세요.
 # https://www.acmicpc.net/problem/2293
+n,k = map(int, input().split())
+coin = []
+for i in range(n):
+    coin.append(int(input()))
+
+coin.sort() # 오름차순 정렬
+
+dp = [0 for i in range(k+1)]
+dp[0] = 1   # 동전을 1개만 쓴 경우
+
+for i in coin:
+    for j in range(i, k+1):
+        if j-i >= 0:
+            dp[j] = dp[j] + dp[j-i]
+
+print(dp[k])
--- 풀이/Jihoon/todo/[다익스트라]지름길.py
+++ 풀이/Jihoon/todo/[다익스트라]지름길.py
@@ -1,6 +1,1 @@
-<<<<<<< HEAD
 # https://www.acmicpc.net/problem/1446
-
-=======
-# https://www.acmicpc.net/problem/1446
->>>>>>> 4f108429e9afb7eb36e03dca54a03488cf5edfe3
