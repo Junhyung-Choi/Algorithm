519f6c8e77d91d5d505b4ec5a965eeec1c65cd42
Junhyung Choi <junhyung9571@naver.com>
2023-11-20 21:14:12 +0900

    Revert "[DP]동전1 (2293)"

--- 풀이/Jihoon/todo/[DP]동전1.py
+++ 풀이/Jihoon/todo/[DP]동전1.py
@@ -1,20 +1,18 @@
 # 본인이 선택한 언어로 풀이해주세요.
 # https://www.acmicpc.net/problem/2293
 n,k = map(int, input().split())
-coins = []
+coin = []
 for i in range(n):
-    coins.append(int(input()))
+    coin.append(int(input()))
 
-coins.sort() # 오름차순 정렬
+coin.sort() # 오름차순 정렬
 
 dp = [0 for i in range(k+1)]
 dp[0] = 1   # 동전을 1개만 쓴 경우
 
-for coin in coins:
-    for j in range(coin, k+1):
-        if j-coin >= 0:
-            dp[j] = dp[j] + dp[j-coin]
-            print(dp)
-    print("\n")
+for i in coin:
+    for j in range(i, k+1):
+        if j-i >= 0:
+            dp[j] = dp[j] + dp[j-i]
 
 print(dp[k])
