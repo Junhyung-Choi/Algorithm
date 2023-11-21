2bf6c5b99bc01f5837f31ec80c506ce0fae50398
Junhyeong_Choi <junhyung9571@naver.com>
2023-11-21 10:36:40 +0900

    Solve: 신기한 소수

--- 풀이/Junhyeong/todo/[DFS]신기한 소수.py
+++ 풀이/Junhyeong/todo/[DFS]신기한 소수.py
@@ -1,1 +1,27 @@
 # https://www.acmicpc.net/problem/2023
+import math
+
+def isPrime(number):
+    if number < 2 : return False
+    for i in range(2, int(math.sqrt(number)) + 1):
+        if number % i == 0:
+            return False
+    return True
+
+digit = int(input())
+iNumbers = [[2,3,5,7]]
+
+if digit == 1:
+    for i in iNumbers[0]:
+        print(i)
+
+for d in range(1, digit):
+    iNumbers.append([])
+    for front in iNumbers[d-1]:
+        for behind in range(10):
+            candidate = front * 10 + behind
+            if isPrime(candidate):
+                if d == digit - 1:
+                    print(candidate)
+                else:
+                    iNumbers[d].append(candidate)
