b161de218b3cd5d3fd59685bc4c06e29bf168df7
Junhyeong_Choi <junhyung9571@naver.com>
2023-11-28 18:26:14 +0900

    Chore : Move Directory

--- 풀이/Junhyeong/done/BOJ/[DFS]신기한 소수.py
+++ 풀이/Junhyeong/done/BOJ/[DFS]신기한 소수.py
@@ -8,20 +8,26 @@ def isPrime(number):
             return False
     return True
 
+def checkInterestingNumber(number):
+    string_number = str(number)
+    for i in range(len(string_number) - 1):
+        if not int(string_number[:i+1]) in iNumbers:
+            return False
+    return isPrime(number)
+
 digit = int(input())
 iNumbers = [[2,3,5,7]]
 
-if digit == 1:
-    for i in iNumbers[0]:
-        print(i)
 
 for d in range(1, digit):
     iNumbers.append([])
     for front in iNumbers[d-1]:
-        for behind in range(10):
+        for behind in [1,3,5,7,9]:
             candidate = front * 10 + behind
             if isPrime(candidate):
                 if d == digit - 1:
                     print(candidate)
-                else:
-                    iNumbers[d].append(candidate)
+                iNumbers[d].append(candidate)
+
+for row in iNumbers:
+    print(row)
